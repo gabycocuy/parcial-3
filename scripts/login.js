86cf2eb3a7000200337cc5227ebcd549efef7e56
Gabycocuy <gcocuymarin@gmail.com>
2025-06-12 01:11:16 -0500

    login con local storage

---
+++ scripts/login.js
@@ -0,0 +1,31 @@
+const loginForm = document.getElementById("loginForm")
+
+function ingresarUsuario(e){
+    e.preventDefault()
+    const emailValor = document.getElementById("email").value
+    const passwordValor = document.getElementById("password").value
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+    const existeUsuario = usuarios.find(usuario => usuario.email === emailValor)
+
+    if(!existeUsuario){
+        alert("El usuario no existe, por favor registralo")
+        loginForm.reset()
+        return
+    }
+
+    if(existeUsuario.password !== passwordValor){
+        alert("Contraseña incorrecta")
+        loginForm.reset()
+        return
+    }
+    
+    const usuarioLogueado = {
+        nombreCompleto: existeUsuario.nombre + " " + existeUsuario.apellido,
+        email: existeUsuario.email,
+    }
+
+    localStorage.setItem("logueado", JSON.stringify(usuarioLogueado))
+    window.location.href = "index.html"
+}
+loginForm.addEventListener("submit", ingresarUsuario)
